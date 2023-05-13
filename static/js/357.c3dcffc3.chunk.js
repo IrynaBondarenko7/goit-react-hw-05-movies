46a0270ed0a5daf8df128daaa2fa597d0175716b
IrynaBondarenko7 <113060476+IrynaBondarenko7@users.noreply.github.com>
2023-05-13 13:32:02 +0000

    Deploying to gh-pages from @ IrynaBondarenko7/goit-react-hw-05-movies@bb871246c14eb111945c353c97de650275bbe9e4 🚀

--- static/js/357.c3dcffc3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[357],{357:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"Reviews"})}}}]);
-//# sourceMappingURL=357.c3dcffc3.chunk.js.map
